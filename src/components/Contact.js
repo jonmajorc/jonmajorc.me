c496bd44550660606e35de482f0832ff5a6adc68
Jon Major Condon <jmajorcondon@gmail.com>
2018-06-03 17:57:08 -0500

    quote around inputs and textarea

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,15 +17,21 @@ class Notes extends React.Component {
           <form className="form" action="">
             <div className="form__group">
               <label htmlFor="name">Name:</label>
+              <span className="input-quote">{'"'}</span>
               <input type="text" />
+              <span className="input-quote">{'"'}</span>
             </div>
             <div className="form__group">
               <label htmlFor="subject">Subject:</label>
+              <span className="input-quote">{'"'}</span>
               <input type="text" />
+              <span className="input-quote">{'"'}</span>
             </div>
             <div className="form__group">
               <label htmlFor="message">Message:</label>
+              <span className="input-quote">{'"'}</span>
               <textarea />
+              <span className="input-quote">{'"'}</span>
             </div>
           </form>
         </div>
