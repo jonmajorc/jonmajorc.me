eb54934fd1e52b9156d41a6748f619172ecaebc3
Jon Major Condon <jmajorcondon@gmail.com>
2019-06-03 19:52:12 -0500

    Minor styling adjustments

--- src/components/BlogDate.js
+++ src/components/BlogDate.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types'
 
 const BlogDate = props => {
   return (
-    <span className={cx('BlogDate', props.classNames)}>
+    <span className={cx('BlogDate', props.className)}>
       {props.date || 'No date present'}
     </span>
   )
 }
 
 BlogDate.propTypes = {
-  className: PropTypes.oneOf(['blog-post__header_date']),
+  className: PropTypes.oneOf(['blog-post__header__date']),
 }
 
 export default BlogDate
