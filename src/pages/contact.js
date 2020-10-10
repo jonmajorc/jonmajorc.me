74093708d9c179f26e55dc7d1b80df5ecc2e4dac
Jon Major Condon <jmajorcondon@gmail.com>
2020-10-09 20:52:37 -0500

    contact form wip

---
+++ src/pages/contact.js
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+
+/***************************************************************************\
+  modules
+\***************************************************************************/
+import { jsx, Styled } from 'theme-ui'
+import { graphql } from 'gatsby'
+
+/***************************************************************************\
+  components 
+\***************************************************************************/
+import SEO from '../components/seo'
+import Layout from '../components/main-layout'
+import { ContactForm } from '../components/contact-form'
+
+const Contact = ({ data, location }) => {
+  let siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Contact" />
+      <Styled.h4>Contact</Styled.h4>
+      <ContactForm />
+    </Layout>
+  )
+}
+
+export default Contact
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
